6e855dacede35a743f3d59d2b7ef40aeac29c1e1
kiryls <kiryl92@gmail.com>
2024-01-15 22:18:34 +0100

    refactored return values

--- src/utils/ipc/sem/sem.c
+++ src/utils/ipc/sem/sem.c
@@ -1,11 +1,8 @@
 #include "sem.h"
 
-#include "../../io/logger/error_logger.h"
+#include <sys/semaphore.h>
 
-// #include <semaphore.h>
-// #include <sys/fcntl.h>
-// #include <sys/semaphore.h>
-// #include <sys/stat.h>
+#include "../../io/logger/error_logger.h"
 
 #define SEM_PERMS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
 
@@ -14,8 +11,7 @@ sem_t *sem_create(const char *sem_name, unsigned int initial_value) {
                                 0xffffffff, initial_value);
 
     if (semaphore == SEM_FAILED) {
-        ERRLOG("sem_create()");
-        exit(EXIT_FAILURE);
+        return NULL;
     }
 
     return semaphore;
@@ -25,8 +21,7 @@ sem_t *sem_get(const char *sem_name) {
     sem_t *semaphore = sem_open(sem_name, O_RDWR);
 
     if (semaphore == SEM_FAILED) {
-        ERRLOG("sem_get()");
-        exit(EXIT_FAILURE);
+        return NULL;
     }
 
     return semaphore;
@@ -46,8 +41,4 @@ void sem_release(const char *sem_name) {
     }
 }
 
-void sem_remove(const char *sem_name) {
-    if (sem_unlink(sem_name) < 0) {
-        ERRLOG("sem_remove()");
-    }
-}
+void sem_remove(const char *sem_name) { sem_unlink(sem_name); }
--- src/utils/ipc/sem/sem.h
+++ src/utils/ipc/sem/sem.h
@@ -1,6 +1,7 @@
 #ifndef SEM_H
 #include <fcntl.h>
 #include <semaphore.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -8,10 +9,29 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+/**
+ * returns NULL or a valid semaphore pointer
+ */
 sem_t *sem_create(const char *sem_name, unsigned int initial_value);
+
+/**
+ * returns NULL or a valid semaphore pointer
+ */
 sem_t *sem_get(const char *sem_name);
+
+/**
+ * does exit if sem_wait() returns -1
+ */
 void sem_acquire(const char *sem_name);
+
+/**
+ * does exit if sem_wait() returns -1
+ */
 void sem_release(const char *sem_name);
+
+/**
+ * returns nothing
+ */
 void sem_remove(const char *sem_name);
 
 #endif  // !SEM_H
