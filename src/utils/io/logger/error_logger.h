0ff60468c9144e7557c60d6afbc5827fab386ca4
kiryls <kiryl92@gmail.com>
2024-01-15 22:04:05 +0100

    extended length of errormsg to 1024

--- src/utils/io/logger/error_logger.h
+++ src/utils/io/logger/error_logger.h
@@ -2,7 +2,7 @@
 
 #define ERRLOG(arg)                                                          \
     {                                                                        \
-        char errormsg[256];                                                  \
+        char errormsg[1024];                                                 \
         sprintf(errormsg, "ERROR: in file '%s' at line %d for %s", __FILE__, \
                 __LINE__, arg);                                              \
         perror(errormsg);                                                    \
