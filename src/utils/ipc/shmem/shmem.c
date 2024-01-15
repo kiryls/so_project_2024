d8755841554dea541c4a5b0904ba0be0029c98ff
kiryls <kiryl92@gmail.com>
2024-01-15 22:19:53 +0100

    small refacoring

--- src/utils/ipc/shmem/shmem.c
+++ src/utils/ipc/shmem/shmem.c
@@ -10,20 +10,22 @@
 void *shmem_create(const char *shmem_name, size_t size) {
     int shmem_fd = shm_open(shmem_name, O_CREAT | O_EXCL | O_RDWR, 0666);
     if (shmem_fd < 0) {
-        ERRLOG("shmem_create().shm_open()");
-        exit(EXIT_FAILURE);
+        char arg[1024];
+        sprintf(arg,
+                "ERROR: in file %s, line %d - couldn't shm_open(%s, O_CREAT)\n",
+                __FILE__, __LINE__, shmem_name);
+        ERRLOG(arg);
+        return NULL;
     }
 
     if (ftruncate(shmem_fd, size) < 0) {
-        ERRLOG("shmem_create().ftruncate()");
-        exit(EXIT_FAILURE);
+        return NULL;
     }
 
     void *shmem_ptr =
         mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shmem_fd, 0);
     if (shmem_ptr == MAP_FAILED) {
-        ERRLOG("shmem_create().mmap()");
-        exit(EXIT_FAILURE);
+        return NULL;
     }
 
     return shmem_ptr;
@@ -32,23 +34,20 @@ void *shmem_create(const char *shmem_name, size_t size) {
 void *shmem_get(const char *shmem_name, size_t size) {
     int shmem_fd = shm_open(shmem_name, O_RDWR, 0);
     if (shmem_fd < 0) {
-        ERRLOG("shmem_get().shm_open()");
-        // exit(EXIT_FAILURE);
+        char arg[1024];
+        sprintf(arg, "ERROR: in file %s, line %d - couldn't shm_open(%s)\n",
+                __FILE__, __LINE__, shmem_name);
+        ERRLOG(arg);
         return NULL;
     }
 
     void *shmem_ptr =
         mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shmem_fd, 0);
     if (shmem_ptr == MAP_FAILED) {
-        ERRLOG("shmem_get().mmap()");
-        exit(EXIT_FAILURE);
+        return NULL;
     }
 
     return shmem_ptr;
 }
 
-void shmem_remove(const char *shmem_name) {
-    if (shm_unlink(shmem_name) < 0) {
-        ERRLOG("shmem_remove()");
-    }
-}
+void shmem_remove(const char *shmem_name) { shm_unlink(shmem_name); }
--- src/utils/ipc/shmem/shmem.h
+++ src/utils/ipc/shmem/shmem.h
@@ -1,9 +1,21 @@
 #ifndef SHMEM_H
 
+#include <stdbool.h>
 #include <stdlib.h>
 
+/**
+ * returns NULL or a valid shared memory pointer
+ */
 void *shmem_create(const char *shmem_name, size_t size);
+
+/**
+ * returns NULL or a valid shared memory pointer
+ */
 void *shmem_get(const char *shmem_name, size_t size);
+
+/**
+ * returns true if shm_unlink succeded, false otherwise
+ */
 void shmem_remove(const char *shmem_name);
 
 #endif  // !SHMEM_H
